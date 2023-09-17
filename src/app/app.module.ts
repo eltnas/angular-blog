a35d432fcfc2cbeebaf261a377a7155c057cfffa
Elton C. Nascimento <eltnas@outlook.com>
2023-09-17 12:21:19 -0300

    Projeto de blog criado em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuTitleComponent,
+    MenuNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
